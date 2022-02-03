1027068c450f022c4bac77dfbf0c6623bedb1bcf
zhangli2946 <zhangli2946@gmail.com>
2022-02-03 22:05:02 +0800

    release 1.0.1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go4s/factorize
+
+go 1.17
--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 var (
-    BinVer  = "v1.0.0"
+    BinVer  = "v1.0.1"
     BinName = "factorize"
     BinYear = "2022"
     BinAuth = "zhangli2946 <zhangli2946@gmail.com>"
